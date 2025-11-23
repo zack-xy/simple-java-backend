feaf5bf546245b2e335300828f988d763e77a8b5
zack zheng <zack_zhengxiyun@163.com>
2025-11-23 13:44:28 +0800

    init

---
+++ src/main/java/com/example/simplejavabackend/SimpleJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplejavabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJavaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleJavaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplejavabackend/SimpleJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplejavabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJavaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
